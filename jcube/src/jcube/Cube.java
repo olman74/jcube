43f4a95c8a1b63929d66270dd901a393c0bb20c7
Laurent Laffont <laurent.laffont@gmail.com>
2012-11-21 15:33:36 +0100

    Implémentation de la lecture du fichier pour une seule ligne / face

--- jcube/src/jcube/Cube.java
+++ jcube/src/jcube/Cube.java
@@ -1,13 +1,34 @@
 package jcube;
 
-public class Cube {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-	public static Cube fromTextFile(String string) {
-		return new Cube();
+public class Cube {
+	private String filePath;
+
+	public static Cube fromTextFile(String filepath) {
+		Cube cube = new Cube();
+		return cube.setFilePath(filepath);
 	}
+
+	private Cube setFilePath(String filepath) {
+		this.filePath = filepath;
+		return this;
+	}
+
 
 	public Integer numberOfFaces() {
 		return 1;
+	}
+
+	public String titreFace(int i) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(new File(this.filePath)));
+		String line = reader.readLine();
+		reader.close();
+		
+		return line.substring(2);
 	}
 
 }
--- jcube/src/jcube/CubeFromOneLineTextFile.java
+++ jcube/src/jcube/CubeFromOneLineTextFile.java
@@ -2,14 +2,27 @@ package jcube;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CubeFromOneLineTextFile {
+	private Cube cube;
 
+	@Before
+	public void createCubeFromTextFile() {
+		this.cube = Cube.fromTextFile("templates/one-line-cube.txt");		
+	}
+	
+	
 	@Test
 	public void cubeShouldHaveOneFace() {
-		Cube cube = Cube.fromTextFile("templates/one-line-cube.txt");
-		assertEquals(new Integer(1), cube.numberOfFaces());	
+		assertEquals(new Integer(1), this.cube.numberOfFaces());	
 	}
 
+	@Test
+	public void titreFaceShouldBeCommentSortirDuCoursPlusTot() throws IOException {
+		assertEquals("Comment sortir du cours plus t™t", this.cube.titreFace(0));
+	}
 }

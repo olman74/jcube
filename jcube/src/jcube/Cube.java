47f005d3fe629f9df0bfd972058aa6cc98c089a4
jongr <jongr@192.168.155.73>
2012-11-23 16:41:37 +0100

    Test avec XML<->TXT

--- jcube/src/jcube/Cube.java
+++ jcube/src/jcube/Cube.java
@@ -5,6 +5,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
 public class Cube {
 	private String faceOne;
 
@@ -30,4 +38,13 @@ public class Cube {
 		Cube otherCube = (Cube)other;
 		return this.faceOne.equals(otherCube.faceOne);
 	}
+
+	public  XMLDocument toSVG(String string) throws Exception {
+		XMLDocument doc = (new XMLDocument()).loadXMLFile("templates/cube.svg");
+		Element blocNode = doc.getFirstNodeFromXPath("//tspan[contains(text(), \"$BLOCK1\")]");
+		blocNode.setTextContent(this.faceOne);
+		return doc;
+	}
+
+	
 }
--- jcube/src/jcube/CubeFromOneLineTextFile.java
+++ jcube/src/jcube/CubeFromOneLineTextFile.java
@@ -5,7 +5,14 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.xpath.XPathExpressionException;
+
 import org.junit.Test;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 public class CubeFromOneLineTextFile {	
 	@Test
@@ -16,4 +23,14 @@ public class CubeFromOneLineTextFile {
 		Cube actualCube = Cube.fromTextFile("templates/one-line-cube.txt");		
 		assertEquals(expectedCube, actualCube);
 	}
+	
+	@Test
+	public void theLineInTheFileTextShouldBeInSVGFile()throws Exception
+	{
+		Cube actualCube = Cube.fromTextFile("templates/one-line-cube.txt");
+		XMLDocument doc = (XMLDocument) actualCube.toSVG("templates/cube.svg");
+		assertTrue(doc.match("//tspan[contains(text(), \"Comment sortir du cours plus tot\")]"));
+	}
 }
+
+

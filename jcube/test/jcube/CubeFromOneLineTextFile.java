667f5b532d80a7b782de87c77db1c8d53a685095
jongr <jongr@192.168.155.73>
2012-11-23 17:22:24 +0100

    Sources séparés des test dans dossier

--- jcube/test/jcube/CubeFromOneLineTextFile.java
+++ jcube/test/jcube/CubeFromOneLineTextFile.java
@@ -10,24 +10,31 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.xpath.XPathExpressionException;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
-public class CubeFromOneLineTextFile {	
+public class CubeFromOneLineTextFile {
+	
+	private Cube actualCube;
+	
+	@Before
+	public void loadCubeFromTextFile() throws IOException
+	{
+		this.actualCube = Cube.fromTextFile("templates/one-line-cube.txt");	
+	}
+	
 	@Test
 	public void cubeShouldBeOneFaceWithCommentSortirDuCoursPlusTot() throws IOException {
 		Cube expectedCube = (new Cube())
 				.addFace("Comment sortir du cours plus tot");
-		
-		Cube actualCube = Cube.fromTextFile("templates/one-line-cube.txt");		
 		assertEquals(expectedCube, actualCube);
 	}
 	
 	@Test
 	public void theLineInTheFileTextShouldBeInSVGFile()throws Exception
 	{
-		Cube actualCube = Cube.fromTextFile("templates/one-line-cube.txt");
 		XMLDocument doc = (XMLDocument) actualCube.toSVG("templates/cube.svg");
 		assertTrue(doc.match("//tspan[contains(text(), \"Comment sortir du cours plus tot\")]"));
 	}

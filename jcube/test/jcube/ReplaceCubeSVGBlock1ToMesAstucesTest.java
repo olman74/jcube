e200757c85a878800514b66e00aeb04654b03b2a
Alex <Alex@Alex-pc>
2012-11-27 16:59:39 +0100

    Test Pour rajouter dans le SVG l'astuce de la première face

--- jcube/test/jcube/ReplaceCubeSVGBlock1ToMesAstucesTest.java
+++ jcube/test/jcube/ReplaceCubeSVGBlock1ToMesAstucesTest.java
@@ -13,18 +13,20 @@ import org.junit.Test;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
-public class ReplaceCubeSVGBlock1ToMesAstucesTest {
+public class ReplaceCubeSVGBlock1ToMesAstucesTest 
+{
 
-@Test
-public void replaceNodeContentShouldBeMesAstuces() throws SAXException, IOException, ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException, XPathExpressionException {
-XMLDocument cubeSVG = (new XMLDocument()).loadXMLFile("templates/cube.svg");
-Element blocNode = cubeSVG.getFirstNodeFromXPath("//tspan[contains(text(), \"$BLOCK1\")]");
-blocNode.setTextContent("Mes astuces");
-
-XMLDocument readCubeSVG = (new XMLDocument()).loadXMLString(cubeSVG.asXMLString());
-assertTrue(readCubeSVG.match("//tspan[contains(text(), \"Mes astuces\")]"));
+	@Test
+	public void replaceNodeContentShouldBeMesAstuces() throws Exception
+	{
+		XMLDocument cubeSVG = (new XMLDocument()).loadXMLFile("templates/cube.svg");
+		Element blocNode = cubeSVG.getFirstNodeFromXPath("//tspan[contains(text(), \"$BLOCK1\")]");
+		blocNode.setTextContent("Mes astuces");
+		XMLDocument readCubeSVG = (new XMLDocument()).loadXMLString(cubeSVG.asXMLString());
+		assertTrue(readCubeSVG.match("//tspan[contains(text(), \"Mes astuces\")]"));
+	}
 }
 
-}
+
 
 
---
+++ jcube/test/jcube/ReplaceCubeSVGText1ToMesAstucesTest.java
@@ -0,0 +1,11 @@
+package jcube;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.w3c.dom.Element;
+
+public class ReplaceCubeSVGText1ToMesAstucesTest 
+{
+
+}
